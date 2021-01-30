dd171ae888569387ce607e2eeebf103cff099720
Bohdan00735 <bodya00735@gmail.com>
2021-01-30 11:13:40 +0200

    Initial commit

---
+++ src/test/java/mygroup/demo/RrrServerApplicationTests.java
@@ -0,0 +1,13 @@
+package mygroup.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RrrServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
